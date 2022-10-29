9061ae751b7850b36322274c75baf55c7ae77213
dnnijmlinn <den.koski@icloud.com>
2022-10-29 14:44:44 +0300

    vitsi-bot-in-telegram
    vitsi-bot-in-telegram

---
+++ main.py
@@ -0,0 +1,38 @@
+from gettext import dpgettext
+import requests
+import random
+import telebot
+from telebot import types 
+from bs4 import BeautifulSoup as b
+
+URL = 'https://www.vitsienvitsit.fi/vitsit/'
+API_KEY = '5615366447:AAFEIuf152Z074LakXDd7UmCU-hyOoCqSVA'
+def parser(url):
+    r = requests.get(url)
+
+    soup = b(r.text, 'html.parser')
+    anekdots = soup.find_all('p', class_='vitsiUnselectable')
+
+    return [c.text for c in anekdots]
+
+list_of_jokes = parser(URL)
+random.shuffle(list_of_jokes)
+
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "kirjoita jotain, niin lähetän vitsin")
+
+
+@bot.message_handler(content_types=['text'])
+def jokes(message):
+    if message.text.isdigit:
+        bot.send_message(message.chat.id, list_of_jokes[0])
+        del list_of_jokes[0]
+    else:
+        bot.send_message(message.chat.id, 'write something and Ill send you a joke.')
+
+bot.polling(none_stop=True)
+
+
